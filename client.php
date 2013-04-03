45333df73fee91febd53b2a90ecf3c4dee2197df
Александр <alexandr@alexandr.(none)>
2013-04-03 04:14:38 +0300

    fixes

--- client.php
+++ client.php
@@ -1,47 +1,60 @@
 <?php
 
+$mysqli = mysqli_connect('localhost', 'root', '', 'gearman');
 # Создание клиентского объекта
 $gmclient= new GearmanClient();
 
 # Указание сервера по умолчанию (localhost).
 $gmclient->addServer('localhost', 4730);
 
-echo "Sending job\n";
+$gmclient->setCompleteCallback('taskCompleted');
 
-# Отправка задания обратно
-do
-{
-  $result = $gmclient->doBackground("ordersSimul", "Hello!");
-
-  # Проверка на различные возвращаемые пакеты и ошибки.
-  switch($gmclient->returnCode())
-  {
-    case GEARMAN_WORK_DATA:
-      echo "Data: $result\n";
-      break;
-    case GEARMAN_WORK_STATUS:
-      list($numerator, $denominator)= $gmclient->doStatus();
-      echo "Status: $numerator/$denominator complete\n";
-      break;
-    case GEARMAN_WORK_FAIL:
-      echo "Failed\n";
-      exit;
-    case GEARMAN_SUCCESS:
-        echo $result;
-      break;
-    default:
-      echo "RET: " . $gmclient->returnCode() . "\n";
-      exit;
-  }
+function taskCompleted($task) {
+    echo "Task # " . $task->jobHandle() . " has been completed <br />";
 }
-while($gmclient->returnCode() != GEARMAN_SUCCESS);
+
+$query = 'SELECT * FROM orders';
+$result = mysqli_query($mysqli, $query);
+$i = 6;
+echo "Sending ".$i." jobs...<br />";
+
+$start_profiler = microtime(true);
+while ($i) {
+    $row = array($i, $i, 0);
+    $row2 = array(13131, 24242);
+    $result = $gmclient->doBackground("ordersSimul", serialize($row));
+    //$result = $gmclient->doBackground("ordersSimul", serialize($row2));
+    //$result = $gmclient->addTask("ordersSimul", serialize($row));
+    //$gmclient->runTasks();
+
+
+    $i--;
+    $done = false;
+    do {
+        usleep(500000);
+        $stat = $gmclient->jobStatus($result);
+        if (!$stat[0]) {
+            $done = true;
+        }
+        echo "Is Working?: " . ($stat[1] ? "true" : "false") . ", mails sent : " . $stat[2] . ", total: " . $stat[3] . "<br />";
+    } while (!$done);
+
+    flush();
+}
+$stop_profiler = (round((microtime(true) - $start_profiler), 3) * 1);
+echo $stop_profiler . ' seconds elapsed for sending all tasks.<br /><br />';
+
+class GearmanClient
+{
+    //public function
+
+}
 
 
 
 function getOrder() {
-	//$mysqli = mysqli_connect('localhost', 'root', '', 'gearman');
-	//$query = 'SELECT * FROM orders';
-	//$result = mysqli_query($mysqli, $query);
+	//
+
 	//$orders = mysqli_fetch_assoc($result);
 	$order = generateRandomString(10);
 	return $order;
--- worker.php
+++ worker.php
@@ -15,56 +15,30 @@ $gmworker->addFunction("ordersSimul", "ordersSimul");
 print "Waiting for job...\n";
 while($gmworker->work())
 {
-	print "New task found.. Starting work...\n";
-	$start_profiler = microtime(true);
+
 	if ($gmworker->returnCode() != GEARMAN_SUCCESS)
 	{
 		print "return_code: " . $gmworker->returnCode() . "\n";
 		break;
 	}
-	$stop_profiler = round((microtime(true) - $start_profiler));
-	echo $stop_profiler . ' seconds elapsed for tast: ';
-   // die();
+
 }
 
-function reverse_fn($job)
+function ordersSimul($job)
 {
-  echo "Received job: " . $job->handle() . "\n";
 
-  $workload = $job->workload();
-  $workload_size = $job->workloadSize();
+    print "New task found: ".__FUNCTION__.".\nStarting work...\n\n";
+    $orders = $job->workload();
+    $orders = unserialize($orders);
+    $totalMails = 50;
 
-  echo "Workload: $workload ($workload_size)\n";
+    // Do manipulation with order
+    for ($sentMail=0; $sentMail<=$totalMails; $sentMail++) {
+        usleep(100000);
+        $job->sendStatus($sentMail, $totalMails);
+    }
 
-  # Этот цикл не является необходимым, но показывает как выполняется работа
-  for ($x= 0; $x < $workload_size; $x++)
-  {
-    echo "Sending status: " . ($x + 1) . "/$workload_size complete\n";
-    $job->sendStatus($x, $workload_size);
-    sleep(1);
-  }
-
-  $result= strrev($workload);
-  echo "Result: $result\n";
-
-  # Возвращаем, когда необходимо отправить результат обратно клиенту.
-  return $result;
-}
-
-# Гораздо более простая и менее подробная версия вышеприведенной функции выглядит так:
-function reverse_fn_fast($job)
-{
-  return strrev($job->workload());
-}
-
-function ordersSimul() {
-
-	$orders = getOrders();
-	foreach ($orders as $order) {
-		// Do manipulation with order
-		sleep(1);
-	}
-    return 'Order convertered.';
+    return 'Orders convertered:' . $orders;
 }
 
 function getOrders() {
